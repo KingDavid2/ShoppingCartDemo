c04ec514c5e369f1a4d0b4ca3f789268c34f4ebc
David Nahum <nausito@gmail.com>
2022-03-12 15:48:31 -0700

    Calculate the tax rate of the shopping cart with multiple items

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -3,9 +3,11 @@
 # ShoppingCart class, holds and manage the user products
 class ShoppingCart
   attr_accessor :products
+  attr_accessor :tax_rate
 
-  def initialize
+  def initialize(tax_rate: 0.125)
     @products = []
+    @tax_rate = tax_rate
   end
 
   def add_products(*products)
@@ -14,5 +16,13 @@ class ShoppingCart
 
   def total_price
     @products.map(&:price).inject(:+).round(2)
+  end
+
+  def tax_amount
+    (total_price * @tax_rate).round(2)
+  end
+
+  def total_price_with_tax
+    (total_price + tax_amount).round(2)
   end
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -2,17 +2,19 @@ require_relative '../lib/product'
 require_relative '../lib/shopping_cart'
 
 describe ShoppingCart do
-  let(:product) { Product.new('Dove Soap', 39.99) }
-  let(:cart) { ShoppingCart.new }
+  let(:product1) { Product.new('Dove Soap', 39.99) }
+  let(:product2) { Product.new('Axe Deo', 99.99) }
+  let(:tax_rate) { 0.125 }
+  let(:cart) { ShoppingCart.new(tax_rate: tax_rate) }
 
   describe 'Add products to the shopping cart' do
     before do
-      5.times { cart.add_products(product) }
+      5.times { cart.add_products(product1) }
     end
 
     it 'The shopping cart should contain 5 Dove Soaps each with a unit price of 39.99' do
-      product_names = cart.products.select { |product|  product.name == 'Dove Soap' }
-      product_prices = cart.products.select { |product|  product.price == 39.99 }
+      product_names = cart.products.select { |product1|  product1.name == 'Dove Soap' }
+      product_prices = cart.products.select { |product1|  product1.price == 39.99 }
 
       expect(product_names.count).to eq(5)
       expect(product_prices.count).to eq(5)
@@ -25,13 +27,13 @@ describe ShoppingCart do
 
   describe 'Add additional products of the same type to the shopping cart' do
     before do
-      5.times { cart.add_products(product) }
-      3.times { cart.add_products(product) }
+      5.times { cart.add_products(product1) }
+      3.times { cart.add_products(product1) }
     end
 
     it 'The shopping cart should contain 8 Dove Soaps each with a unit price of 39.99' do
-      product_names = cart.products.select { |product|  product.name == 'Dove Soap' }
-      product_prices = cart.products.select { |product|  product.price == 39.99 }
+      product_names = cart.products.select { |product1|  product1.name == 'Dove Soap' }
+      product_prices = cart.products.select { |product1|  product1.price == 39.99 }
 
       expect(product_names.count).to eq(8)
       expect(product_prices.count).to eq(8)
@@ -39,6 +41,37 @@ describe ShoppingCart do
 
     it 'The shopping cart\'s total price should equal 319.92' do
       expect(cart.total_price).to eq(319.92)
+    end
+  end
+
+  describe 'Calculate the tax rate of the shopping cart with multiple items' do
+    before do
+      2.times { cart.add_products(product1) }
+      2.times { cart.add_products(product2) }
+    end
+
+    it 'The shopping cart should contain 2 Dove Soaps each with a unit price of 39.99' do
+      product_names = cart.products.select { |product1|  product1.name == 'Dove Soap' }
+      product_prices = cart.products.select { |product1|  product1.price == 39.99 }
+
+      expect(product_names.count).to eq(2)
+      expect(product_prices.count).to eq(2)
+    end
+
+    it 'The shopping cart should contain 2 Axe Deos each with a unit price of 99.99' do
+      product2_names = cart.products.select { |product1|  product1.name == 'Axe Deo' }
+      product2_prices = cart.products.select { |product1|  product1.price == 99.99 }
+
+      expect(product2_names.count).to eq(2)
+      expect(product2_prices.count).to eq(2)
+    end
+
+    it 'And the total sales tax amount for the shopping cart should equal 35.00' do
+      expect(cart.tax_amount).to eq(35.00)
+    end
+
+    it 'The shopping cart\'s total price should equal 319.92' do
+      expect(cart.total_price_with_tax).to eq(314.96)
     end
   end
 end
